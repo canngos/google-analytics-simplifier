af0526948a499c0d6cab3d1953a2f6f03e1fa251
Can Baturlar <baturlarcan@gmail.com>
2023-11-09 21:21:31 +0300

    initialized

---
+++ src/main/java/com/analytics/googlesimplifier/GoogleSimplifierApplication.java
@@ -0,0 +1,13 @@
+package com.analytics.googlesimplifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSimplifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSimplifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analytics/googlesimplifier/GoogleSimplifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analytics.googlesimplifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleSimplifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
